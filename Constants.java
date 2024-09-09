c6a4f44a826b1f9d0d9f245e34e1583b3d650ed0
Suhani Mathur <139855595+suhani2706@users.noreply.github.com>
2024-09-09 23:59:54 +0530

    added files

---
+++ Constants.java
@@ -0,0 +1,15 @@
+package com.Studyopedia;
+
+
+//Constants.java
+public class Constants {
+ public static final String INDEX = "1. Parentheses Combination Generator\n2. Digit Sum Calculator";
+ public static final String TASKNUMBER = "Enter task number or type 'exit' to quit:";
+ public static final String EXIT = "Exiting the program...";
+ public static final String TASK1EXECUTION = "Executing task 1: Valid Parentheses Combination Generator";
+ public static final String TASK1INPUT = "Please enter the string for combination generation:";
+ public static final String TASK2EXECUTION = "Executing task 2: Digit Sum Calculator";
+ public static final String TASK2INPUT = "Please enter the number to find digit sum:";
+ public static final String PLEASEINPUT = "Please input a valid non-negative number.";
+}
+
